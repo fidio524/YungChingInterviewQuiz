51791502d3d88ee0604e8ec1bc8962d529057947
jeff850524 <jeff850524@yahoo.com.tw>
2024-02-18 20:00:13 +0800

    完成DbContext、Startup初始設定，並建立CustomersMode;

---
+++ YungChingInterviewQuiz/DataAccess/NorthwindDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YungChingInterviewQuiz.Models;
+
+namespace YungChingInterviewQuiz.DataAccess
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public NorthwindDbContext(DbContextOptions<NorthwindDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CustomersModel> CustomersModel { get; set; }
+    }
+}
---
+++ YungChingInterviewQuiz/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YungChingInterviewQuiz.DataAccess;
+
+namespace YungChingInterviewQuiz
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<NorthwindDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("NorthwindDb")));
+            services.AddControllers();
+
+        }
+    }
+}
